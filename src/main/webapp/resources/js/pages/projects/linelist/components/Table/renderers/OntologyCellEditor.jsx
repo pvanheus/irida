94ff4a41e48ef0f9e0c36a2a3f8bf3d2c7647171
Josh Adam <josh.adam@canada.ca>
2019-09-25 15:15:24 -0500

    Working on a popout editor

--- src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/OntologyCellEditor.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/OntologyCellEditor.jsx
@@ -1,29 +1,55 @@
 import React from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
 import axios from "axios";
-import { Select, Spin } from "antd";
-import { grey7 } from "../../../../../../styles/colors";
+import { Form, Select, Spin } from "antd";
+import { grey1, grey2, grey7 } from "../../../../../../styles/colors";
 import _ from "lodash";
 
 const { Option } = Select;
+const PADDING = 30;
+const LABEL_OFFSET = 29;
+
+const OntoInner = styled.div`
+  padding: ${PADDING / 2}px ${PADDING}px 0 ${PADDING}px;
+  background-color: ${grey1};
+  border: 1px solid ${grey2};
+  border-radius: 3px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  position: absolute;
+  left: ${props => `${props.rect.x - PADDING}px`};
+  top: ${props => `${props.rect.y - PADDING / 2 - LABEL_OFFSET}px`};
+  width: ${props => `${props.rect.width + 2 * PADDING}px`};
+  height: ${props => `${props.rect.height + 2 * PADDING + LABEL_OFFSET}px`};
+  z-index: 500;
+`;
 
 export class OntologyCellEditor extends React.Component {
   constructor(props) {
+    console.log(props);
     super(props);
     this.state = {
       value: props.value,
       dataSource: [],
       fetching: false
     };
+
+    this.rect = props.eGridCell.getBoundingClientRect();
     this.lastFetchId = 0;
     this.onSearch = _.debounce(this.onSearch, 500);
     this.stopEditing = props.stopEditing;
     this.selectRef = React.createRef();
+
+    this.elm = document.querySelector("#onto-wrapper");
+    if (this.elm === null) {
+      this.elm = document.createElement("div");
+      document.body.appendChild(this.elm);
+    }
   }
 
   getValue = () => this.state.value;
 
   afterGuiAttached() {
-    // this.selectRef.current.focus();
     this.selectRef.current.rcSelect.inputRef.click();
   }
 
@@ -47,35 +73,44 @@ export class OntologyCellEditor extends React.Component {
   };
 
   render() {
-    return (
-      <Select
-        ref={this.selectRef}
-        loading={this.state.fetching}
-        showSearch
-        showArrow={false}
-        style={{ width: "100%", margin: 0 }}
-        defaultActiveFirstOption={false}
-        notFoundContent={
-          this.state.fetching ? (
-            <div>
-              <Spin size="small" /> Fetching ...
-            </div>
-          ) : null
-        }
-        filterOption={false}
-        value={this.state.value}
-        onSearch={this.onSearch}
-        onChange={value => {
-          this.setState({ value }, this.stopEditing);
-        }}
-      >
-        {this.state.dataSource.map(item => (
-          <Option key={item.value}>
-            {item.value}
-            <br /> <small style={{ color: grey7 }}>{item.ontologyUri}</small>
-          </Option>
-        ))}
-      </Select>
+    return createPortal(
+      <OntoInner id="onto-inner" rect={this.rect}>
+        <Form layout="vertical">
+          <Form.Item label={this.props.colDef.headerName}>
+            <Select
+              ref={this.selectRef}
+              loading={this.state.fetching}
+              showSearch
+              showArrow={false}
+              style={{ width: "100%", margin: 0 }}
+              defaultActiveFirstOption={false}
+              getPopupContainer={() => document.querySelector("#onto-inner")}
+              notFoundContent={
+                this.state.fetching ? (
+                  <div>
+                    <Spin size="small" /> Fetching ...
+                  </div>
+                ) : null
+              }
+              filterOption={false}
+              value={this.state.value}
+              onSearch={this.onSearch}
+              onChange={value => {
+                this.setState({ value }, this.stopEditing);
+              }}
+            >
+              {this.state.dataSource.map(item => (
+                <Option key={item.value}>
+                  {item.value}
+                  <br />{" "}
+                  <small style={{ color: grey7 }}>{item.ontologyUri}</small>
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Form>
+      </OntoInner>,
+      this.elm
     );
   }
 }
